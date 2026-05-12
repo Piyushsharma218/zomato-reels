dd2ded86cf4a29a1f83a11ce473749a2ef976569
Piyushsharma218 <sharmasatyam524@gmail.com>
2026-05-12 15:24:36 +0530

    created login and logout controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieparser=require("cookie-parser")
 const app = express();
+const authroutes=require("./routes/auth.routes.js")
 
 app.use(express.json());
 app.use(cookieparser())
@@ -8,5 +9,7 @@ app.use(cookieparser())
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.use("/api/auth",authroutes)
 
 module.exports = app;
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,3 +6,7 @@ const router = express.Router();
 // user auth APIs
 router.post('/user/register', authController.registerUser)
 router.post('/user/login', authController.loginUser)
+router.get('/user/logout', authController.logoutUser)
+
+
+module.exports=router
