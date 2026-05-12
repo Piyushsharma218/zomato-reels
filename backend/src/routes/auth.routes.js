ce25f6e4658f8939267cf6fc14bf75ae8fbbc5ee
Piyushsharma218 <sharmasatyam524@gmail.com>
2026-05-12 15:53:38 +0530

    created created register login and logout for food partner

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -8,5 +8,9 @@ router.post('/user/register', authController.registerUser)
 router.post('/user/login', authController.loginUser)
 router.get('/user/logout', authController.logoutUser)
 
+// food partner auth api
+router.post('/food-partner/register', authController.registerFoodPartner)
+router.post('/food-partner/login', authController.loginFoodPartner)
+router.get('/food-partner/logout', authController.logoutFoodPartner)
 
 module.exports=router
