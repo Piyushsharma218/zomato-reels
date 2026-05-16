a4a2f34036a8ddee267f8074579692f2b603623f
Piyushsharma218 <sharmasatyam524@gmail.com>
2026-05-17 00:13:44 +0530

    created food partner route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const cookieparser=require("cookie-parser")
-const app = express();
-const authRoutes=require("./routes/auth.routes.js")
-const foodRoutes=require("./routes/food.routes.js")
+const cookieparser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes.js");
+const foodRoutes = require("./routes/food.routes.js");
+const foodPartnerRoutes = require("./routes/food-partner.routes.js");
+const cors = require("cors");
 
+const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(cookieparser())
+app.use(cookieparser());
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.use("/api/auth",authRoutes)
-app.use("/api/food",foodRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/food", foodRoutes);
+app.use("/api/food-partner", foodPartnerRoutes);
 
 module.exports = app;
--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -9,12 +9,12 @@ const upload = multer({
   storage: multer.memoryStorage(),
 });
 
-router.post(
-  "/",
-  authMiddleware.authFoodPartnerMiddleware,
-  upload.single("video"),
-  foodController.createFood,
-);
+router.post("/",authMiddleware.authFoodPartnerMiddleware,upload.single("video"),foodController.createFood,);
+
+
 router.get("/", authMiddleware.authUserMiddleware, foodController.getFoodItems);
+
+
+
 
 module.exports = router;
